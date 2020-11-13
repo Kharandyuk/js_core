e82a11319ae2f601826973365401510689ece31a
kharandyuk <ykharandyuk@n-ix.com>
2020-11-13 09:27:34 +0200

    master

--- HW1/script.js
+++ HW1/script.js
@@ -1,1 +1,29 @@
+//T2
 console.log("Kharandyuk");
+//T3
+let first = "first"
+let second = "second";
+alert("first variable has value:" + first);
+alert("second variable has value:" + second);
+first = second;
+alert("But now first variable has value:" + first);
+alert("and second variable has value:" + second);
+//T4
+let firstObject = {
+    _string: "This is string", 
+    _number: 1,
+    _boolean: false,
+    _undefined:undefined,
+    _null: null
+ };
+ //console.log(firstObject);
+ //T5
+ let isAdult = confirm("Are you mature enough?")
+ console.log(isAdult);
+ 
+ //T6
+ let name = "Yuriy";
+ let surname = "Kharandyuk";
+ let group = "Lv-548";
+ let birthDate = "27/07/1982";
+ let isMarried = true;
