bd156414ce909e410af36b64ac5addcb21de126e
kharandyuk <ykharandyuk@n-ix.com>
2020-11-14 17:45:39 +0200

    master

--- HW1/script.js
+++ HW1/script.js
@@ -25,5 +25,33 @@ let firstObject = {
  let name = "Yuriy";
  let surname = "Kharandyuk";
  let group = "Lv-548";
- let birthDate = "27/07/1982";
+ let birthYear = 1982;
  let isMarried = true;
+ console.log(typeof name);
+ console.log(typeof surname);
+ console.log(typeof group);
+ console.log(typeof birthYear);
+ console.log(typeof isMarried);
+ console.log(birthYear);
+ console.log(isMarried);
+ console.log(name);
+ console.log(surname);
+ console.log(group);
+ let _null = null;
+ let _undefined;
+ console.log(typeof _null);
+ console.log(typeof _undefined);
+ 
+ //T7
+ let login = prompt("Please enter your login", "");
+ let email = prompt("Please enter your email", "");
+ let pwd = prompt("Please enter your password", "");
+ alert("Dear "+login+", your email is "+ email+", your password is "+pwd);
+ 
+ //T8
+ let hour = 60*60;
+ let day = 24*hour;
+ let month = 30*day;
+ console.log(hour);
+ console.log(day);
+ console.log(month);
--- HW2/script.js
+++ HW2/script.js
@@ -14,7 +14,58 @@ let res3 =Boolean(x)||y; // Допишіть код, необхідно вико
 console.log(res3); // true
 console.log(typeof res3); // "boolean"
 
-let res4 = parseInt("" + Boolean(x)+y);// Допишіть код, необхідно використовувати змінні x і y
+let res4 = parseInt("" + Boolean(x));// Допишіть код, необхідно використовувати змінні x і y
 console.log(res4); // NaN
 console.log(typeof res4); // "number"
+
+//T2
+let num = prompt("Please enter your number", "");
+if ((num>=0)&&(num%2 ==0)){
+    console.log("Number "+num+" is positive and even");  
+}else {console.log("Number "+num+" is NOT positive and even");  }
+if (num%7 ==0){
+    console.log("Number "+num+"is multiples of 7");  
+}else {console.log("Number "+num+" is NOT multiples of 7");  }
+
+//T3
+let _arr = [1, "two", true, null];
+alert( "Our array has " +_arr.length +"" );
+_arr[4] = prompt("Please enter value of new array element", "");
+alert("Fifth element of array is "+_arr[4]);
+_arr.shift();
+alert(_arr);
+
+//T4
+let cities = ["Rome", "Lviv", "Warsaw"]; 
+console.log(cities.join('*'));
+
+//T5
+let isAdult = prompt("How old are you?", "");
+
+if (isAdult>=18){alert("Ви досягли повнолітнього віку");}
+    else if(isAdult>10){alert("Ви ще не досягли повнолітнього віку");}
+            else{alert("Ви ще надто молоді");}
+
+//T6
+let a = parseFloat(prompt("please enter length of first side of triangle", ""));
+let b = parseFloat(prompt("please enter length of second side of triangle", ""));
+let c = parseFloat(prompt("please enter length of third side of triangle", ""));
+if(isNaN(a)||isNaN(b)||isNaN(c)||(a <=0)||(b <=0)||(c <=0)){alert("Incorrect data");}
+else{
+    let p = (a+b+c)/2;
+    let s= (p*(p - a)*(p - b)*(p - c))** (1/2);
+    console.log("Square of the triange is: "+s.toFixed(3));    
+}
+if((a**2 + b**2 == c**2)||(a**2 + c**2 == b**2)||(b**2 + c**2 == a**2)){console.log("you've entered right-angled triangle"); }
+else {console.log("you've entered NOT right-angled triangle");}
+
+//T7
+let todayDate = new Date();
+let currentHour = parseInt(todayDate.getHours());
+
+if((currentHour>= 5)&&(currentHour< 11)){console.log("Доброго ранку!"); }
+else if((currentHour>= 11)&&(currentHour< 17)){console.log("Доброго дня!");} 
+    else if((currentHour>= 17)&&(currentHour< 23)){console.log("Доброго вечора!");}
+        else {console.log("Доброї ночі!");}
+
 
